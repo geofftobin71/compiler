907974c5b68f236d26e410d700a036cb508f27b1
geofftobin71 <geofftobin71@gmail.com>
2022-07-25 05:09:50 +0000

    Chapter 24

--- src/object.h
+++ src/object.h
@@ -2,25 +2,36 @@
 #define _object_h_
 
 #include "common.h"
+#include "chunk.h"
 #include "value.h"
 
 #define OBJ_TYPE(value)        (AS_OBJ(value)->type)
 
+#define IS_FUNCTION(value)     isObjType(value, OBJ_FUNCTION)
 #define IS_STRING(value)       isObjType(value, OBJ_STRING)
 
+#define AS_FUNCTION(value)     ((ObjFunction*)AS_OBJ(value))
 #define AS_STRING(value)       ((ObjString*)AS_OBJ(value))
 #define AS_CSTRING(value)      (((ObjString*)AS_OBJ(value))->chars)
 
 typedef enum
 {
-    OBJ_STRING,
+  OBJ_FUNCTION,
+  OBJ_STRING,
 } ObjType;
 
 struct Obj
 {
-  ObjType type;
+  ObjType     type;
   struct Obj* next;
 };
+
+typedef struct {
+  Obj         obj;
+  int         arity;
+  Chunk       chunk;
+  ObjString*  name;
+} ObjFunction;
 
 struct ObjString
 {
@@ -30,12 +41,13 @@ struct ObjString
   uint32_t  hash;
 };
 
+ObjFunction* newFunction();
 ObjString* takeString(char* chars, int length);
 ObjString* copyString(const char* chars, int length);
 void printObject(Value value);
 
 static inline bool isObjType(Value value, ObjType type) {
-    return IS_OBJ(value) && AS_OBJ(value)->type == type;
+  return IS_OBJ(value) && AS_OBJ(value)->type == type;
 }
 
 #endif

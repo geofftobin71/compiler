db6f26be68ff242b7587d875a4fc360a8e4cc8a2
geofftobin71 <geofftobin71@gmail.com>
2022-07-25 06:04:10 +0000

    Chapter 24

--- src/vm.c
+++ src/vm.c
@@ -24,10 +24,23 @@ static void runtimeError(const char* format, ...)
   va_end(args);
   fputs("\n", stderr);
 
-  CallFrame* frame = &vm.frames[vm.frameCount - 1];
-  size_t instruction = frame->ip - frame->function->chunk.code - 1;
-  int line = frame->function->chunk.lines[instruction];
-  fprintf(stderr, "[line %d] in script\n", line);
+  for(int i = vm.frameCount - 1; i >= 0; i--)
+  {
+    CallFrame* frame = &vm.frames[i];
+    ObjFunction* function = frame->function;
+    size_t instruction = frame->ip - function->chunk.code - 1;
+    fprintf(stderr, "[line %d] in ", function->chunk.lines[instruction]);
+    if(function->name == NULL)
+    {
+      fprintf(stderr, "script\n");
+    }
+    else
+    {
+      fprintf(stderr, "%s()\n", function->name->chars);
+    }
+  }
+
+
   resetStack();
 }
 
@@ -63,6 +76,18 @@ static Value peek(int distance)
 
 static bool call(ObjFunction* function, int argCount)
 {
+  if(argCount != function->arity)
+  {
+    runtimeError("Expected %d arguments but got %d.", function->arity, argCount);
+    return false;
+  }
+
+  if(vm.frameCount == FRAMES_MAX)
+  {
+    runtimeError("Stack overflow.");
+    return false;
+  }
+
   CallFrame* frame = &vm.frames[vm.frameCount++];
   frame->function = function;
   frame->ip = function->chunk.code;
@@ -287,7 +312,18 @@ static InterpretResult run()
         }
       case OP_RETURN:
         {
-          return INTERPRET_OK;
+          Value result = pop();
+          vm.frameCount--;
+          if(vm.frameCount == 0)
+          {
+            pop();
+            return INTERPRET_OK;
+          }
+
+          vm.stackTop = frame->slots;
+          push(result);
+          frame = &vm.frames[vm.frameCount - 1];
+          break;
         }
     }
   }

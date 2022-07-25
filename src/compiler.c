6164869b7842017827f2e8f487540c7526a0e48a
geofftobin71 <geofftobin71@gmail.com>
2022-07-25 05:19:25 +0000

    Chapter 24

--- src/compiler.c
+++ src/compiler.c
@@ -55,14 +55,15 @@ typedef enum
   TYPE_SCRIPT
 } FunctionType;
 
-typedef struct
+typedef struct Compiler
 {
-  ObjFunction* function;
-  FunctionType type;
+  struct Compiler*  enclosing;
+  ObjFunction*      function;
+  FunctionType      type;
 
-  Local       locals[UINT8_COUNT];
-  int         localCount;
-  int         scopeDepth;
+  Local             locals[UINT8_COUNT];
+  int               localCount;
+  int               scopeDepth;
 } Compiler;
 
 Parser parser;
@@ -211,6 +212,7 @@ static void patchJump(int offset)
 
 static void initCompiler(Compiler* compiler, FunctionType type)
 {
+  compiler->enclosing = current;
   compiler->function = NULL;
   compiler->type = type;
   compiler->localCount = 0;
@@ -236,6 +238,7 @@ static ObjFunction* endCompiler()
   }
 #endif
 
+  current = current->enclosing;
   return function;
 }
 
@@ -530,6 +533,7 @@ static uint8_t parseVariable(const char* errorMessage)
 
 static void markInitialized()
 {
+  if(current->scopeDepth == 0) return;
   current->locals[current->localCount - 1].depth = current->scopeDepth;
 }
 
@@ -562,6 +566,29 @@ static void block()
   }
 
   consume(TOKEN_RIGHT_BRACE, "Expect '}' after block.");
+}
+
+static void function(FunctionType type)
+{
+  Compiler compiler;
+  initCompiler(&compiler, type);
+  beginScope(); 
+
+  consume(TOKEN_LEFT_PAREN, "Expect '(' after function name.");
+  consume(TOKEN_RIGHT_PAREN, "Expect ')' after parameters.");
+  consume(TOKEN_LEFT_BRACE, "Expect '{' before function body.");
+  block();
+
+  ObjFunction* function = endCompiler();
+  emitBytes(OP_CONSTANT, makeConstant(OBJ_VAL(function)));
+}
+
+static void funDeclaration()
+{
+  uint8_t global = parseVariable("Expect function name.");
+  markInitialized();
+  function(TYPE_FUNCTION);
+  defineVariable(global);
 }
 
 static void varDeclaration()
@@ -715,7 +742,11 @@ static void synchronize()
 
 static void declaration()
 {
-  if(match(TOKEN_VAR))
+  if(match(TOKEN_FUN))
+  {
+    funDeclaration();
+  }
+  else if (match(TOKEN_VAR))
   {
     varDeclaration();
   }

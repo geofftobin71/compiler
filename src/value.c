b3797fc1fd1d804a556f62d9e3c87cf20c49d8d0
geofftobin71 <geofftobin71@gmail.com>
2022-07-26 06:47:47 +0000

    Chapter 30

--- src/value.c
+++ src/value.c
@@ -33,6 +33,24 @@ void freeValueArray(ValueArray* array)
 
 void printValue(Value value)
 {
+#ifdef NAN_BOXING
+  if(IS_BOOL(value))
+  {
+    printf(AS_BOOL(value) ? "true" : "false");
+  }
+  else if (IS_NIL(value))
+  {
+    printf("nil");
+  }
+  else if (IS_NUMBER(value))
+  {
+    printf("%g", AS_NUMBER(value));
+  }
+  else if (IS_OBJ(value))
+  {
+    printObject(value);
+  }
+#else
   switch(value.type)
   {
     case VAL_BOOL:
@@ -42,10 +60,14 @@ void printValue(Value value)
     case VAL_NUMBER: printf("%g", AS_NUMBER(value)); break;
     case VAL_OBJ: printObject(value); break;
   }
+#endif
 }
 
 bool valuesEqual(Value a, Value b)
 {
+#ifdef NAN_BOXING
+  return a == b;
+#else
   if(a.type != b.type) return false;
   switch(a.type)
   {
@@ -55,4 +77,5 @@ bool valuesEqual(Value a, Value b)
     case VAL_OBJ:    return AS_OBJ(a) == AS_OBJ(b);
     default:         return false; // Unreachable.
   }
+#endif
 }
